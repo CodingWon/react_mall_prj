e5796107e68c6179c9cc20f69a0f6a722a3a3a2d
kjw <kingcoding2298@gmail.com>
2024-07-10 06:13:46 +0900

    MainPage에 lazy 로딩 추가

--- src/router/root.js
+++ src/router/root.js
@@ -1,10 +1,14 @@
+import { Suspense , lazy } from "react";
 import MainPage from "../pages/MainPage";
-
 const {createBrowserRouter} = require("react-router-dom");
+
+const Loading = <div>Loading ....</div>
+const Main = lazy(() => import("../pages/MainPage"))
+
 const root = createBrowserRouter([
     {
         path: '',
-        element: <MainPage/>
+        element: <Suspense fallback={Loading}><Main/></Suspense>
     }
 
 ])
