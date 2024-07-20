3c0ee1d8456d24d62fb840ff20a11ef37f992165
kjw <kingcoding2298@gmail.com>
2024-07-20 16:22:35 +0900

    prefix 오류 수정

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -6,7 +6,7 @@ export const API_SERVER_HOST = "http://localhost:8080";
 const prefix = `${API_SERVER_HOST}/api/todo`;
 
 export const getOne = async (tno) => {
-    const res = await axios.get(`prefix/${tno}`);
+    const res = await axios.get(`${prefix}/${tno}`);
 
     return res.data;
 }
